1745eb3d48a8909214b5c945626ca1d921568e4e
Chris Bacon <chris@dunelm.org.uk>
2016-03-08 11:44:42 +0000

    Add CommonSettings and ServiceApiSettings
    With tests.

---
+++ src/Google.Api.Gax/Api/Gax/ServiceSettingsBase.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using System;
+
+namespace Google.Api.Gax
+{
+    /// <summary>
+    /// Common settings for all services.
+    /// </summary>
+    public abstract class ServiceSettingsBase
+    {
+        private TimeSpan? _timeout;
+
+        /// <summary>
+        /// If not null, a timeout for all RPC calls. If null or unset, the RPC method default timeout will be used.
+        /// </summary>
+        public TimeSpan? Timeout
+        {
+            get { return _timeout; }
+            set
+            {
+                if (value != null && value.Value.Ticks <= 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Timeout must be positive and cannot be zero");
+                }
+                _timeout = value;
+            }
+        }
+
+        /// <summary>
+        /// If not null, the clock used to calculate RPC deadlines. If null or unset, the <see cref="SystemClock"/> is used.
+        /// </summary>
+        /// <remarks>
+        /// This is primarily only to be set for testing.
+        /// In production code generally leave this unset to use the <see cref="SystemClock"/>.
+        /// </remarks>
+        public IClock Clock { get; set; }
+
+        protected virtual T CloneInto<T>(T settings) where T : ServiceSettingsBase
+        {
+            settings.Timeout = Timeout;
+            settings.Clock = Clock;
+            return settings;
+        }
+    }
+}
---
+++ test/Google.Api.Gax.Tests/ServiceSettingsBaseTest.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using Moq;
+using System;
+using Xunit;
+
+namespace Google.Api.Gax.Tests
+{
+    public class ServiceSettingsBaseTest
+    {
+        class TestSettings : ServiceSettingsBase
+        {
+            public TestSettings Clone()
+            {
+                return CloneInto(new TestSettings());
+            }
+        }
+
+        [Fact]
+        public void DefaultToNulls()
+        {
+            var settings = new TestSettings();
+            Assert.Null(settings.Timeout);
+            Assert.Null(settings.Clock);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(1)]
+        public void Timeout_Valid(int? millis)
+        {
+            var timeout = millis == null ? default(TimeSpan?) : TimeSpan.FromMilliseconds(millis.Value);
+            var settings = new TestSettings { Timeout = timeout };
+            Assert.Equal(timeout, settings.Timeout);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Timeout_Invalid(int? millis)
+        {
+            var timeout = millis == null ? default(TimeSpan?) : TimeSpan.FromMilliseconds(millis.Value);
+            Assert.Throws<ArgumentOutOfRangeException>(() => new TestSettings { Timeout = timeout });
+        }
+
+        [Fact]
+        public void Clone()
+        {
+            var clock = new Mock<IClock>();
+            var timeout = TimeSpan.FromSeconds(1);
+            var settings = new TestSettings
+            {
+                Timeout = timeout,
+                Clock = clock.Object,
+            };
+            var clonedSettings = settings.Clone();
+            Assert.NotSame(settings, clonedSettings);
+            Assert.Equal(timeout, clonedSettings.Timeout);
+            Assert.Equal(clock.Object, clonedSettings.Clock);
+        }
+
+    }
+}
