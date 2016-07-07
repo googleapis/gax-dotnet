74f1d29379c3fa66b2c44daf77f35dd025dfe89b
Jon Skeet <jonskeet@google.com>
2016-07-07 09:56:37 +0100

    Move generated protobuf code to new Google.Api.CommonProtos package
    GAX does not depend on this directly, but gRPC-based APIs will need to.
    (Changes to gcloud-dotnet will happen when I've published the new package to myget.)

--- myget.bat
+++ myget.bat
@@ -2,6 +2,8 @@ call dnvm use 1.0.0-rc1-update1 -r clr
 IF ERRORLEVEL 1 EXIT /B 1
 call dnu restore
 IF ERRORLEVEL 1 EXIT /B 1
+call dnu build src\Google.Api.CommonProtos
+IF ERRORLEVEL 1 EXIT /B 1
 call dnu build src\Google.Api.Gax
 IF ERRORLEVEL 1 EXIT /B 1
 call dnu build src\Google.Api.Gax.Rest
@@ -15,6 +17,8 @@ IF ERRORLEVEL 1 EXIT /B 1
 call dnx -p test\Google.Api.Gax.Tests test
 IF ERRORLEVEL 1 EXIT /B 1
 call dnx -p test\Google.Api.Gax.Rest.Tests test
+IF ERRORLEVEL 1 EXIT /B 1
+call dnu pack src\Google.Api.CommonProtos
 IF ERRORLEVEL 1 EXIT /B 1
 call dnu pack src\Google.Api.Gax
 IF ERRORLEVEL 1 EXIT /B 1
