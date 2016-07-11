719d02bc46d9117ed3ce208d57ade99ae5d6a1e2
Jon Skeet <jonskeet@google.com>
2016-07-11 14:20:07 +0100

    Add more diagnostics in myget

--- myget.bat
+++ myget.bat
@@ -1,3 +1,4 @@
+@echo on
 call dnvm use 1.0.0-rc1-update1 -r clr || goto error
 call dnu restore || goto error
 
@@ -5,22 +6,29 @@ set PROJECTS=src\Google.Api.CommonProtos src\Google.Api.Gax src\Google.Api.Gax.R
 set TESTS=test\Google.Api.Gax.Tests test\Google.Api.Gax.Rest.Tests
 
 for %%P in (%PROJECTS%) do (
+  echo Building %%P
   call dnu build --configuration %Configuration% %%P || goto error
 )
 
 for %%P in (%TESTS%) do (
+  echo Testing %%P
   call dnu build --configuration %Configuration% %%P || goto error
   dnx --configuration %Configuration% -p %%P test || goto error
 )
 
 for %%P in (%PROJECTS%) do (
+  echo Packing %%P
   call dnu pack --configuration %Configuration% %%P || goto error
 )
+
+echo Done!
 
 goto end
 
 :error
 echo Build failed due to error. Please address.
+echo Pausing for build logs to catch up
+timeout /T 5
 exit /b 1
 
 :end
