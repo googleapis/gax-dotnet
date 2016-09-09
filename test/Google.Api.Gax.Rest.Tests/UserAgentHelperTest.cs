a560bb0fd5224ae8ea9f47027f62c7d28233fbbd
Jon Skeet <jonskeet@google.com>
2016-09-09 11:30:19 +0100

    Add a user agent helper to the REST version of GAX
    This avoids us baking logic into multiple libraries
    (Storage+Bigquery).

---
+++ test/Google.Api.Gax.Rest.Tests/UserAgentHelperTest.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using Xunit;
+
+namespace Google.Api.Gax.Rest.Tests
+{
+    public class UserAgentHelperTest
+    {
+        [Fact]
+        public void GetDefaultUserAgent()
+        {
+            // Always 1.0.0, because we don't declare a version nubmer in project.json for tests.
+            Assert.Equal("gcloud-dotnet/1.0.0", UserAgentHelper.GetDefaultUserAgent(typeof(UserAgentHelperTest)));
+        }
+    }
+}
