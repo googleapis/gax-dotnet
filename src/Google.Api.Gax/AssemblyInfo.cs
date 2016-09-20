0602b60ea096779598b6fc82f68cf94d20145b62
Jon Skeet <jonskeet@google.com>
2016-09-20 16:14:41 +0100

    Enable easy faking of ApiCall<,>.
    This could make it reasonably easy to bypass gRPC if we wanted to, constructing a FooClientImpl with appropriate API calls but no gRPC client. Something for later consideration.

---
+++ src/Google.Api.Gax/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Google.Api.Gax.Testing,PublicKey=0024000004800000940000000602000000240000525341310004000001000100bfa2d20e82583e5f3a47efa518d8a7cda58d6e1b61f51ed3de3900f238fb01e0ca95b3ed1c68147e4865a336b252f070d5207e653c9014d271fc829c18981d1c80d418d300545c3ad0e1ff4d77c596177dbfe0a588fff57306f1d6ba655de3cfa520ad777ea73bb7a92fd6027071c94bb12d5871fcd076299de0502bca12e3aa")]
