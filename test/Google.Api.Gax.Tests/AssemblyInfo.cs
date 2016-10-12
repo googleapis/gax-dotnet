74cad02e7f0ceffb27b23607b363c80d1a136789
Jon Skeet <jonskeet@google.com>
2016-10-12 07:45:47 +0100

    Make FakeScheduler less flaky
    We now don't have the flaky "pause" at various points. This does mean that any scheduled actions can easily overlap each other in real time, but it's hard to fix that without flakiness. If it becomes an issue, we can add a configurable pause.
    
    The API is now consistent and more clearly documented, and
    has a couple of "guard" methods to prevent Run being called
    accidentally instead of RunAsync. (I found I consistently made that
    error.)
    
    For some reason they fail - only on Travis - if run in parallel.
    That shouldn't be the case, and I'll file a new issue about it, but
    it can wait for now.

---
+++ test/Google.Api.Gax.Tests/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
