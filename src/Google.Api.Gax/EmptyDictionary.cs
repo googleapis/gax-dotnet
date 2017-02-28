24361db236ccaa59566133dac54a8c81e19c87ac
Chris Bacon <chris@dunelm.org.uk>
2017-02-28 10:38:15 +0000

    Add empty dictionary class (#142)

---
+++ src/Google.Api.Gax/EmptyDictionary.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Google.Api.Gax
+{
+    /// <summary>
+    /// Provides cached instances of empty dictionaries.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+    public static class EmptyDictionary<TKey, TValue>
+    {
+        /// <summary>
+        /// Gets a cached empty <see cref="IDictionary{TKey, TValue}"/>. The returned dictionary is read-only.
+        /// </summary>
+        public static IDictionary<TKey, TValue> Instance { get; } =
+            new ReadOnlyDictionary<TKey, TValue>(new Dictionary<TKey, TValue>(0));
+    }
+}
---
+++ test/Google.Api.Gax.Tests/EmptyDictionaryTest.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using System;
+using Xunit;
+
+namespace Google.Api.Gax.Tests
+{
+    public class EmptyDictionaryTest
+    {
+        [Fact]
+        public void IsCached()
+        {
+            var a1 = EmptyDictionary<int, string>.Instance;
+            var b1 = EmptyDictionary<string, int>.Instance;
+            var a2 = EmptyDictionary<int, string>.Instance;
+            var b2 = EmptyDictionary<string, int>.Instance;
+            Assert.Same(a1, a2);
+            Assert.Same(b1, b2);
+            Assert.NotSame(a1, b1);
+            Assert.NotSame(a2, b2);
+        }
+
+        [Fact]
+        public void IsReadonly()
+        {
+            var a = EmptyDictionary<int, string>.Instance;
+            Assert.Throws<NotSupportedException>(() => a[0] = "Zero");
+        }
+
+        [Fact]
+        public void IsEmpty()
+        {
+            var a = EmptyDictionary<int, string>.Instance;
+            Assert.Equal(0, a.Count);
+        }
+    }
+}
