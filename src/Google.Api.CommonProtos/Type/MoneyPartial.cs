3a7c2cb8bcc77f2e6835f74ffa52534236b62847
Gregory Delpe <gregory@namely.com>
2018-07-30 14:56:18 -0400

    Add DecimalValue property to Google.Type.Money

--- src/Google.Api.CommonProtos/Type/MoneyPartial.cs
+++ src/Google.Api.CommonProtos/Type/MoneyPartial.cs
@@ -12,12 +12,36 @@ namespace Google.Type
     public partial class Money
     {
         /// <summary>
-        /// Converts <see cref="Money"/> value to <see cref="decimal"/>, omitting the <see cref="CurrencyCode"/>.
+        /// The amount of money in <see cref="decimal"/> format.
         /// </summary>
-        /// <returns>The amount as a <see cref="decimal"/>.</returns>
-        public decimal ToDecimal()
-        {
-            return Units + (Nanos / 1_000_000_000M);
+        /// <exception cref="ArgumentOutOfRangeException">The integral part of the decimal must be a valid <see cref="Int64"/>, and the fractional part must have a maximum of 9 digits of precision.</exception>
+        public decimal DecimalValue {
+
+            get => Units + (Nanos / 1_000_000_000M);
+
+            set {
+                decimal integralPart = Math.Truncate(value);
+                long units;
+
+                try
+                {
+                    units = (long)integralPart;
+                }
+                catch (OverflowException ex)
+                {
+                    throw new ArgumentOutOfRangeException($"Unable to convert the value {value.ToString()} to Money.  The integral part of the amount must be a valid System.Int64 value.", ex);
+                }
+
+                decimal fractionalPart = (value - integralPart) * 1_000_000_000M;
+
+                if (fractionalPart != Math.Truncate(fractionalPart))
+                {
+                    throw new ArgumentOutOfRangeException($"Unable to convert the value {value.ToString()} to Money precisely.  The Money type supports a maximum of 9 digits after the decimal.");
+                }
+
+                Units = units;
+                Nanos = (int)fractionalPart;
+            }
         }
     }
 }
