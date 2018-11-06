26bbd1aa43ca5d8ebfab245f3229a638c4dda892
Jon Skeet <jonskeet@google.com>
2018-11-06 13:44:31 +0000

    Additional Task-related extension methods:
    - WaitWithUnwrappedExceptions overloads to match Wait
    - TaskCompletionSource.WithCancellationToken

---
+++ Google.Api.Gax.Tests/TaskCompletionSourceExtensionsTests.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Google.Api.Gax.Tests
+{
+    public class TaskCompletionSourceExtensionsTests
+    {
+        [Fact]
+        public async Task WithCancellationToken_None()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            var task = tcs.WithCancellationToken(CancellationToken.None);
+
+            tcs.SetResult(5);
+            Assert.Equal(5, await task);
+        }
+
+        [Fact]
+        public async Task WithCancellationToken_Uncancelled()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            var source = new CancellationTokenSource();
+            var task = tcs.WithCancellationToken(source.Token);
+
+            tcs.SetResult(5);
+            Assert.Equal(5, await task);
+        }
+
+        [Fact]
+        public async Task WithCancellationToken_CancelledBeforeCreation()
+        {
+            var source = new CancellationTokenSource();
+            source.Cancel();
+            var tcs = new TaskCompletionSource<int>();
+            var task = tcs.WithCancellationToken(source.Token);
+
+            await Assert.ThrowsAsync<TaskCanceledException>(() => task);
+        }
+
+        [Fact]
+        public async Task WithCancellationToken_CancelledAfterCreation()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            var source = new CancellationTokenSource();
+            var task = tcs.WithCancellationToken(source.Token);
+            source.Cancel();
+
+            await Assert.ThrowsAsync<TaskCanceledException>(() => task);
+        }
+    }
+}
---
+++ Google.Api.Gax/TaskCompletionSourceExtensions.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Google.Api.Gax
+{
+    /// <summary>
+    /// Extension methods for <see cref="TaskCompletionSource{TResult}"/>.
+    /// </summary>
+    public static class TaskCompletionSourceExtensions
+    {
+        /// <summary>
+        /// Returns a task from a task completion source, but observing a given cancellation token.
+        /// </summary>
+        /// <typeparam name="TResult">The result type of the task completion source</typeparam>
+        /// <param name="source">The task completion source. Must not be null.</param>
+        /// <param name="cancellationToken">The cancellation token to observe.</param>
+        /// <returns>A task that will complete when <paramref name="source"/> completes, but
+        /// will observe <paramref name="cancellationToken"/> for cancellation.</returns>
+        public static Task<TResult> WithCancellationToken<TResult>(this TaskCompletionSource<TResult> source, CancellationToken cancellationToken)
+        {
+            GaxPreconditions.CheckNotNull(source, nameof(source));
+            if (!cancellationToken.CanBeCanceled)
+            {
+                return source.Task;
+            }
+            return Wait();
+
+            async Task<TResult> Wait()
+            {
+                using (cancellationToken.Register(() => source.TrySetCanceled()))
+                {
+                    return await source.Task.ConfigureAwait(false);
+                }
+            }
+        }
+    }
+}
