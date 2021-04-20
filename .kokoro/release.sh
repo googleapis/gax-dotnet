0de92e636f7481696d3d9de8a6fbe47e70c41aa9
Amanda Tarafa Mas <atarafamas@google.com>
2021-04-20 10:25:03 +0100

    ci: Call populatesecrets.sh from the right folder.

--- .kokoro/release.sh
+++ .kokoro/release.sh
@@ -6,11 +6,12 @@ SCRIPT=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT")
 
 cd $SCRIPT_DIR
-cd ..
 
 # Make sure secrets are loaded in a well known localtion before running releasetool
 source ./populatesecrets.sh
 populate_all_secrets
+
+cd ..
 
 export NUGET_API_KEY="$(cat "$SECRETS_LOCATION"/google-apis-nuget-api-key)"
 
