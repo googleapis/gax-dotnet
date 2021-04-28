1a1b9a88bb3a7ad55a4c24f695c378c21055a3fc
Jon Skeet <jonskeet@google.com>
2021-04-28 11:46:23 +0100

    Fix autorelease to inline Python detection

--- .kokoro/autorelease.sh
+++ .kokoro/autorelease.sh
@@ -18,7 +18,14 @@ python -m releasetool publish-reporter-script > /tmp/publisher-script
 
 # The publish reporter script uses "python3" which doesn't exist on Windows.
 # Work out what we should use instead.
-PYTHON3=$(source ../toolversions.sh && echo $PYTHON3)
+# Try to detect Python 3. It's quite different between Windows and Linux.
+if which python > /dev/null && python --version 2>&1 | grep -q "Python 3"; then declare -r PYTHON3=python
+elif which py > /dev/null && py -3 --version 2>&1 | grep -q "Python 3"; then declare -r PYTHON3="py -3"
+elif which python3 > /dev/null && python3 --version 2>&1 | grep -q "Python 3"; then declare -r PYTHON3=python3
+else
+  echo "Unable to detect Python 3 installation."
+  exit 1
+fi
 
 # Fix up the publish reporter script using $PYTHON3. We assume this won't
 # be harmful within sed - at the moment it's always "python", "py -3" or "python3".
