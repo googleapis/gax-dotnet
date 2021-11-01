2fd8dba3faf7e1a671691e2c2ebb2945902ef807
Jon Skeet <jonskeet@google.com>
2021-11-01 14:36:06 +0000

    Add integration tests for GrpcCoreAdapter and GrpcNetClientAdapter
    Note that GrpcNetClientAdapter ignores the primary user agent option, so we can't easily test that the options are actually used.

---
+++ Google.Api.Gax.Grpc.IntegrationTests/GrpcCoreAdapterTest.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Copyright 2021 Google LLC
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using Grpc.Core;
+using Xunit;
+using static Google.Api.Gax.Grpc.IntegrationTests.TestService;
+
+namespace Google.Api.Gax.Grpc.IntegrationTests
+{
+    [Collection(nameof(TestServiceFixture))]
+    public class GrpcCoreAdapterTest
+    {
+        private readonly TestServiceFixture _fixture;
+
+        public GrpcCoreAdapterTest(TestServiceFixture fixture) => _fixture = fixture;
+
+        [Fact]
+        public void CreateChannelMakeCall()
+        {
+            var channel = GrpcCoreAdapter.Instance.CreateChannel(_fixture.Endpoint, ChannelCredentials.Insecure, GrpcChannelOptions.Empty);
+            var client = new TestServiceClient(channel);
+            var response = client.DoSimple(new SimpleRequest { Name = "test-call" });
+            Assert.Equal("test-call", response.Name);
+        }
+
+        // If we can propagate *any* custom option, it's pretty reasonable to expect that they'll all work,
+        // at least in terms of the infrastructure.
+        [Fact]
+        public void PrimaryUserAgentOption()
+        {
+            var options = GrpcChannelOptions.Empty.WithPrimaryUserAgent("test-user-agent");
+            var channel = GrpcCoreAdapter.Instance.CreateChannel(_fixture.Endpoint, ChannelCredentials.Insecure, options);
+            var client = new TestServiceClient(channel);
+            var response = client.EchoHeaders(new EchoHeadersRequest());
+            var userAgent = response.Headers["user-agent"].StringValue;
+            Assert.StartsWith("test-user-agent", userAgent);
+        }
+    }
+}
