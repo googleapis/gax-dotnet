4c94fa629ee6637e029f4f827806759131bb96c0
Jon Skeet <jonskeet@google.com>
2021-11-01 14:37:19 +0000

    Add GrpcAdapter.DefaultAdapter property
    This (lazily, and once) checks whether Grpc.Net.Client works - i.e. whether a channel can be constucted - and uses GrpcNetClientAdapter if so. Otherwise, it uses GrpcCoreAdapter. We may well want to tweak the details of this later.

---
+++ Google.Api.Gax.Grpc.IntegrationTests/GrpcAdapterTest.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Copyright 2021 Google LLC
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using Grpc.Core;
+using Xunit;
+using static Google.Api.Gax.Grpc.IntegrationTests.TestService;
+
+namespace Google.Api.Gax.Grpc.IntegrationTests
+{
+    [Collection(nameof(TestServiceFixture))]
+    public class GrpcAdapterTest
+    {
+        private readonly TestServiceFixture _fixture;
+
+        public GrpcAdapterTest(TestServiceFixture fixture) => _fixture = fixture;
+
+        [Fact]
+        public void CreateChannelMakeCall()
+        {
+            var adapter = GrpcAdapter.DefaultAdapter;
+            // This is unfortunate, but required for the test.
+            // ("localhost:12345" is only valid in Grpc.Core; "http://localhost:12345" is only valid in Grpc.Net.Client.)
+            var endpoint = adapter is GrpcNetClientAdapter ? _fixture.HttpEndpoint : _fixture.Endpoint;
+            var channel = adapter.CreateChannel(endpoint, ChannelCredentials.Insecure, GrpcChannelOptions.Empty);
+            var client = new TestServiceClient(channel);
+            var response = client.DoSimple(new SimpleRequest { Name = "test-call" });
+            Assert.Equal("test-call", response.Name);
+        }
+    }
+}
