d59b47b27dd816aa091a5ad49451760da6d98671
Jon Skeet <jonskeet@google.com>
2022-02-23 15:17:40 +0000

    Add Grpc.Gcp code and proto into GAX repo
    Original source: https://github.com/GoogleCloudPlatform/grpc-gcp-csharp
    
    This currently doesn't build, but it's the starting point for the rest of the work.
    
    Not all of the code had a copyright comment, so I added the one from the proto.
    
    Note: everything here has Google copyright.

---
+++ Google.Api.Gax.Grpc/Gcp/ChannelRef.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2018 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Grpc.Core;
+using System.Threading;
+
+namespace Grpc.Gcp
+{
+    /// <summary>
+    /// Keeps record of channel affinity and active streams.
+    /// This class is thread-safe.
+    /// </summary>
+    internal sealed class ChannelRef
+    {
+        private int affinityCount;
+        private int activeStreamCount;
+        private int id;
+
+        public ChannelRef(Channel channel, int id, int affinityCount = 0, int activeStreamCount = 0)
+        {
+            Channel = channel;
+            this.id = id;
+            this.affinityCount = affinityCount;
+            this.activeStreamCount = activeStreamCount;
+        }
+
+        internal Channel Channel { get; }
+        internal int AffinityCount => Interlocked.CompareExchange(ref affinityCount, 0, 0);
+        internal int ActiveStreamCount => Interlocked.CompareExchange(ref activeStreamCount, 0, 0);
+
+        internal int AffinityCountIncr() => Interlocked.Increment(ref affinityCount);
+        internal int AffinityCountDecr() => Interlocked.Decrement(ref affinityCount);
+        internal int ActiveStreamCountIncr() => Interlocked.Increment(ref activeStreamCount);
+        internal int ActiveStreamCountDecr() => Interlocked.Decrement(ref activeStreamCount);
+
+        internal ChannelRef Clone() => new ChannelRef(Channel, id, AffinityCount, ActiveStreamCount);
+    }
+}
+ 
