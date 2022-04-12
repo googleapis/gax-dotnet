dfbd6dd3a4bda7aa0fab0bf8ab49132693f63de8
Jon Skeet <jonskeet@google.com>
2022-04-12 12:36:32 +0100

    test: Add test loggers
    These are copied from https://github.com/GoogleCloudPlatform/functions-framework-dotnet/tree/main/src/Google.Cloud.Functions.Testing
    
    (At some point we might want to create a separate package for these, given how useful they are...)

---
+++ Google.Api.Gax.Grpc.Tests/MemoryLoggerProvider.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Copyright 2022 Google LLC
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Google.Api.Gax.Grpc.Tests
+{
+    /// <summary>
+    /// A logger provider that creates instances of <see cref="MemoryLogger"/>.
+    /// </summary>
+    internal sealed class MemoryLoggerProvider : ILoggerProvider
+    {
+        private readonly ConcurrentDictionary<string, MemoryLogger> _loggersByCategory =
+            new ConcurrentDictionary<string, MemoryLogger>();
+
+        public ILogger CreateLogger(string categoryName) =>
+            _loggersByCategory.GetOrAdd(categoryName, name => new MemoryLogger(name));
+
+        internal void Clear() => _loggersByCategory.Clear();
+
+        /// <summary>
+        /// Returns a list of log entries for the given category name. If no logs have been
+        /// written for the given category, an empty list is returned.
+        /// </summary>
+        /// <param name="categoryName">The category name for which to get log entries.</param>
+        /// <returns>A list of log entries for the given category name.</returns>
+        internal List<TestLogEntry> GetLogEntries(string categoryName) =>
+            _loggersByCategory.TryGetValue(categoryName, out var logger)
+            ? logger.ListLogEntries() : new List<TestLogEntry>();
+
+        public void Dispose()
+        {
+            // No-op
+        }
+    }
+}
---
+++ Google.Api.Gax.Grpc.Tests/TestLogEntry.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Copyright 2022 Google LLC
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace Google.Api.Gax.Grpc.Tests
+{
+    /// <summary>
+    /// A very simplified representation of a log entry, aimed at testing.
+    /// </summary>
+    public sealed class TestLogEntry
+    {
+        private static readonly IReadOnlyList<object> EmptyScopeList = new List<object>().AsReadOnly();
+
+        /// <summary>
+        /// The category name of the log entry.
+        /// </summary>
+        public string CategoryName { get; }
+
+        /// <summary>
+        /// The level of the log entry.
+        /// </summary>
+        public LogLevel Level { get; }
+
+        /// <summary>
+        /// The exception in the log entry, or null if there was no exception.
+        /// </summary>
+        public Exception Exception { get; }
+
+        /// <summary>
+        /// The event ID of the log entry.
+        /// </summary>
+        public EventId EventId { get; }
+
+        /// <summary>
+        /// The message of the log entry.
+        /// </summary>
+        public string Message { get; }
+
+        /// <summary>
+        /// The scopes of the log entry. This is never null, but may be empty.
+        /// </summary>
+        public IReadOnlyList<object> Scopes { get; }
+
+        private TestLogEntry(string categoryName, LogLevel level, EventId eventId, string message, Exception exception, IReadOnlyList<object> scopes) =>
+            (CategoryName, Level, EventId, Message, Exception, Scopes) =
+            (categoryName, level, eventId, message, exception, scopes);
+
+        internal static TestLogEntry Create<TState>(
+            string categoryName, LogLevel logLevel, EventId eventId,
+            TState state, Exception exception, Func<TState, Exception, string> formatter,
+            IExternalScopeProvider scopeProvider)
+        {
+            var message = formatter(state, exception);
+            List<object> scopes = null;
+            scopeProvider.ForEachScope((scope, state) =>
+            {
+                if (scopes is null)
+                {
+                    scopes = new List<object>();
+                }
+                scopes.Add(scope);
+            }, state: (object) null);
+
+            return new TestLogEntry(categoryName, logLevel, eventId, message, exception, scopes?.AsReadOnly() ?? EmptyScopeList);
+        }
+
+        /// <summary>
+        /// Returns a diagnostic form of the log entry, in the form "[Level]: Message",
+        /// to simplify debugging.
+        /// </summary>
+        /// <returns>A diagnostic form of the log entry.</returns>
+        public override string ToString() => $"[{Level}]: {Message}";
+    }
+}
