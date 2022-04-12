29ee868b91f8a280e1d6d5394d8f43048f96268e
Jon Skeet <jonskeet@google.com>
2022-04-12 12:36:32 +0100

    test: Add test loggers
    These are copied from https://github.com/GoogleCloudPlatform/functions-framework-dotnet/tree/main/src/Google.Cloud.Functions.Testing
    
    (At some point we might want to create a separate package for these, given how useful they are...)

---
+++ Google.Api.Gax.Grpc.Tests/LoggerTypeNameHelper.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Copyright 2022 Google LLC
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Google.Api.Gax.Grpc.Tests
+{
+    /// <summary>
+    /// Helper method to determine the category for an ILogger{T}, given the type T.
+    /// This isn't just the full name of the type, because generics are made prettier - but
+    /// the method used isn't easily accessible.
+    /// </summary>
+    internal static class LoggerTypeNameHelper
+    {
+        internal static string GetCategoryNameForType<T>()
+        {
+            var wrapper = new Logger<T>(NoOpLoggerFactory.Instance);
+            return ((NoOpLogger) wrapper.BeginScope(null)).CategoryName;
+        }
+
+        internal static string GetCategoryNameForType(System.Type type)
+        {
+            // Annoyingly, we have to use reflection here...
+            var method = typeof(LoggerTypeNameHelper).GetMethods(BindingFlags.NonPublic | BindingFlags.Static)
+                .Single(method => method.Name == nameof(GetCategoryNameForType) && method.IsGenericMethodDefinition);
+            method = method.MakeGenericMethod(type);
+            return (string) method!.Invoke(null, null) ?? throw new InvalidOperationException("Method returned null");
+        }
+
+        private class NoOpLoggerFactory : ILoggerFactory
+        {
+            internal static ILoggerFactory Instance { get; } = new NoOpLoggerFactory();
+
+            public void AddProvider(ILoggerProvider provider) =>
+                throw new NotImplementedException();
+
+            public ILogger CreateLogger(string categoryName) =>
+                new NoOpLogger(categoryName);
+
+            public void Dispose() =>
+                throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logger which returns itself when BeginScope is called, allowing us
+        /// to get at the category. Yes, this is very weird.
+        /// </summary>
+        private class NoOpLogger : ILogger, IDisposable
+        {
+            internal string CategoryName { get; }
+
+            internal NoOpLogger(string categoryName) =>
+                CategoryName = categoryName;
+
+            public IDisposable BeginScope<TState>(TState state) => this;
+
+            public void Dispose() =>
+                throw new NotImplementedException();
+
+            public bool IsEnabled(LogLevel logLevel) =>
+                throw new NotImplementedException();
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) =>
+                throw new NotImplementedException();
+        }
+    }
+}
---
+++ Google.Api.Gax.Grpc.Tests/MemoryLogger.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * Copyright 2022 Google LLC
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Google.Api.Gax.Grpc.Tests
+{
+    /// <summary>
+    /// Logger implementation that retains log entries in memory, for test purposes.
+    /// </summary>
+    public class MemoryLogger : ILogger
+    {
+        private readonly string _categoryName;
+        private readonly ConcurrentQueue<TestLogEntry> _logEntries = new ConcurrentQueue<TestLogEntry>();
+        private readonly IExternalScopeProvider _scopeProvider = new LoggerExternalScopeProvider();
+
+        /// <summary>
+        /// Creates a logger with the given category name.
+        /// </summary>
+        /// <param name="categoryName">The category name of the logger.</param>
+        public MemoryLogger(string categoryName) => _categoryName = categoryName;
+
+        /// <summary>
+        /// Clears the log entries in this logger.
+        /// </summary>
+        public void Clear()
+        {
+#if NETCOREAPP3_1_OR_GREATER
+            _logEntries.Clear();
+#else
+            // ConcurrentQueue<T>.Clear doesn't exist in .NET Framework. This is the simplest approach...
+            while (_logEntries.TryDequeue(out _)) ;
+#endif
+        }
+
+        /// <summary>
+        /// Creates a list of the log entries in this logger. The returned list is an independent clone
+        /// of the current set of log entries. This call does not affect the state of the logger.
+        /// </summary>
+        /// <returns>A list of log entries in the logger.</returns>
+        public List<TestLogEntry> ListLogEntries() => _logEntries.ToList();
+
+        /// <inheritdoc />
+        public IDisposable BeginScope<TState>(TState state) => _scopeProvider.Push(state);
+
+        /// <summary>
+        /// Returns true for any log level other than None; filtering is expected
+        /// to be provided by other infrastructure.
+        /// </summary>
+        public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None;
+
+        /// <summary>
+        /// Formats the log entry as a <see cref="TestLogEntry"/> and retains it in memory.
+        /// </summary>
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var entry = TestLogEntry.Create(_categoryName, logLevel, eventId, state, exception, formatter, _scopeProvider);
+            _logEntries.Enqueue(entry);
+        }
+    }
+
+    /// <summary>
+    /// Generic implementation of <see cref="ILogger{TCategoryName}"/>.
+    /// </summary>
+    /// <typeparam name="TCategoryName">The type whose name will be the logger's category name.</typeparam>
+    public class MemoryLogger<TCategoryName> : MemoryLogger, ILogger<TCategoryName>
+    {
+        /// <summary>
+        /// Constructs a logger using the type argument as the category name.
+        /// </summary>
+        public MemoryLogger() : base(LoggerTypeNameHelper.GetCategoryNameForType<TCategoryName>())
+        {
+        }
+    }
+}
