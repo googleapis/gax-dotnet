884a6d65289fb2fe34db405242f5252578c87e76
Jon Skeet <jonskeet@google.com>
2022-06-24 08:29:02 +0100

    refactor: Initial refactoring for REGAPIC transcoding
    Isolating the transcoder from RestMethod will allow for simpler
    data-driven testing and a clean way of handling additional bindings
    (as the RestMethod will then just have multiple transcoders).
    
    This commit is relatively minimal - there are further aspects of
    refactoring which will be easier to reason about after this is in.
    
    This commit does not introduce any tests, but these will be added in
    later commits when we get to the core part of transcoding.

---
+++ Google.Api.Gax.Grpc/Rest/HttpRuleTranscoder.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * Copyright 2022 Google LLC
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using Google.Protobuf;
+using Google.Protobuf.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace Google.Api.Gax.Grpc.Rest;
+
+/// <summary>
+/// A transcoder for a single HttpRule, not including any additional bindings.
+/// A <see cref="RestMethod"/> has one or more transcoders, applied in turn until
+/// a match is found with a <see cref="TranscodingOutput"/> result.
+/// </summary>
+internal sealed partial class HttpRuleTranscoder
+{
+    private static readonly FieldType[] TypesIneligibleForQueryStringEncoding = { FieldType.Group, FieldType.Message, FieldType.Bytes };
+    private static readonly HttpMethod s_patchMethod = new HttpMethod("PATCH");
+
+    private readonly Func<IMessage, TranscodingOutput> _contentFactory;
+
+    internal HttpRuleTranscoder(MethodDescriptor method, HttpRule rule)
+    {
+        (string pattern, var httpMethod) = rule.PatternCase switch
+        {
+            HttpRule.PatternOneofCase.Get => (rule.Get, HttpMethod.Get),
+            HttpRule.PatternOneofCase.Post => (rule.Post, HttpMethod.Post),
+            HttpRule.PatternOneofCase.Patch => (rule.Patch, s_patchMethod),
+            HttpRule.PatternOneofCase.Put => (rule.Put, HttpMethod.Put),
+            HttpRule.PatternOneofCase.Delete => (rule.Delete, HttpMethod.Delete),
+            HttpRule.PatternOneofCase.Custom => (rule.Custom.Path, new HttpMethod(rule.Custom.Kind)),
+            _ => throw new ArgumentException("HTTP rule has no pattern")
+        };
+
+        var pathPattern = HttpRulePathPattern.Parse(pattern, method.InputType);
+
+        // TODO: Refactor this to fit the new class structure.
+        _contentFactory = CreateTranscodingContentFactory(method, httpMethod, pathPattern, rule.Body);
+    }
+
+    /// <summary>
+    /// This function creates a GRPC Transcoding lambda with curried method descriptor
+    ///
+    /// GRPC Transcoding returns the parameters of the http request, namely
+    ///  - the path of the uri
+    ///  - the query string parameters
+    ///  - the body
+    /// based on the
+    ///  - method descriptor, specifically the list of input message fields, and the `google.api.http` annotation
+    ///  - the request message
+    /// It is described in the AIP-127 (https://google.aip.dev/127) and the proto comments for google.api.HttpRule
+    /// (https://github.com/googleapis/googleapis/blob/master/google/api/http.proto#L44-L312)
+    /// </summary>
+    /// <returns>A lambda that will provide the transcoding output when the request method is known</returns>
+    private static Func<IMessage, TranscodingOutput> CreateTranscodingContentFactory(MethodDescriptor protoMethod, HttpMethod httpMethod, HttpRulePathPattern pathPattern, string httpBodyPattern)
+    {
+        Func<IMessage, string> pathFactory = pathPattern.Format;
+
+        (bool allFieldsInBody, string bodyName, Func<IMessage, string> bodyFactory) = httpBodyPattern switch
+        {
+            "*" => (true, null, new Func<IMessage, string>(protoRequest => protoRequest.ToString())),
+            "" => (false, null, new Func<IMessage, string>(protoRequest => null)),
+            string name when protoMethod.InputType.FindFieldByName(name) is FieldDescriptor field => (false, name, new Func<IMessage, string>(protoRequest => field.Accessor.GetValue(protoRequest).ToString())),
+            _ => throw new ArgumentException($"Method {protoMethod.Name} in service {protoMethod.Service.Name} has a body parameter {httpBodyPattern} in the 'google.api.http' annotation which is not a field in {protoMethod.InputType.Name}")
+        };
+
+        List<FieldDescriptor> unusedEligibleFields = new List<FieldDescriptor>();
+        if (!allFieldsInBody)
+        {
+            var usedFieldNames = new HashSet<string>(pathPattern.TopLevelFieldNames);
+            if (bodyName != null)
+            {
+                usedFieldNames.Add(bodyName);
+            }
+
+            var queryStringParamsEligibleFields = protoMethod.InputType.Fields.InDeclarationOrder()
+                .Where(f => !TypesIneligibleForQueryStringEncoding.Contains(f.FieldType));
+
+            unusedEligibleFields = queryStringParamsEligibleFields.Where(field => !usedFieldNames.Contains(field.Name)).ToList();
+        }
+
+        return message =>
+        {
+            var path = pathFactory(message);
+            var body = bodyFactory(message);
+
+            var fieldsToEncode = unusedEligibleFields.Where(field => !field.HasPresence || field.Accessor.HasValue(message));
+
+            var queryStringParams = fieldsToEncode.ToDictionary(field => field.JsonName,
+                field => field.Accessor.GetValue(message).ToString());
+
+            return new TranscodingOutput(httpMethod, path, queryStringParams, body);
+        };
+    }
+
+    /// <summary>
+    /// Attempts to use this rule to transcode the given request.
+    /// </summary>
+    /// <param name="request">The request to transcode. Must not be null.</param>
+    /// <returns>The result of transcoding, or null if the rule did not match.</returns>
+    internal TranscodingOutput Transcode(IMessage request) => _contentFactory(request);
+}
---
+++ Google.Api.Gax.Grpc/Rest/TranscodingOutput.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Copyright 2022 Google LLC
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace Google.Api.Gax.Grpc.Rest;
+
+/// <summary>
+/// The result of transcoding a protobuf request using an HttpRule.
+/// This is produced by <see cref="HttpRuleTranscoder"/>.
+/// </summary>
+internal sealed class TranscodingOutput
+{
+    private const string ApplicationJsonMediaType = "application/json";
+
+    internal string UriPath { get; }
+    internal Dictionary<string, string> QueryStringParameters { get; }
+    internal string Body { get; }
+    internal HttpMethod Method { get; }
+
+    internal TranscodingOutput(HttpMethod method, string uriPath, Dictionary<string, string> queryStringParameters, string body) =>
+        (Method, UriPath, QueryStringParameters, Body) =
+        (method, uriPath, new Dictionary<string, string>(queryStringParameters), body);
+
+    internal HttpRequestMessage CreateRequest(string host)
+    {
+        var uriPathWithParams = QueryStringParameters.Any()
+            ? AppendQueryStringParameters(UriPath, QueryStringParameters.OrderBy(kvp => kvp.Key))
+            : UriPath;
+
+        var uri = host is null ? new Uri(uriPathWithParams, UriKind.Relative) : new UriBuilder { Host = host, Path = uriPathWithParams }.Uri;
+
+        var content = Body is null ? null : new StringContent(Body, Encoding.UTF8, ApplicationJsonMediaType);
+
+        return new HttpRequestMessage
+        {
+            RequestUri = uri,
+            Method = Method,
+            Content = content,
+        };
+    }
+
+    /// <summary>
+    /// Merges the uri path and the query string parameters, escaping them.
+    /// Ignores the possibility that the path can already have parameters or contain an anchor (`#`)
+    /// </summary>
+    /// <param name="uriPath">The path component of the service URI</param>
+    /// <param name="queryStringParameters">The parameters to encode in the query string</param>
+    /// <returns>An uri path merged with the encoded query string parameters</returns>
+    private static string AppendQueryStringParameters(string uriPath, IOrderedEnumerable<KeyValuePair<string, string>> queryStringParameters)
+    {
+        var sb = new StringBuilder();
+        sb.Append(uriPath);
+        bool sbHasParameters = false;
+
+        foreach (var kvpNameValue in queryStringParameters)
+        {
+            sb.Append(sbHasParameters ? "&" : "?");
+            sbHasParameters = true;
+            sb.Append(Uri.EscapeDataString(kvpNameValue.Key));
+            sb.Append("=");
+            sb.Append(Uri.EscapeDataString(kvpNameValue.Value));
+        }
+
+        return sb.ToString();
+    }
+}
