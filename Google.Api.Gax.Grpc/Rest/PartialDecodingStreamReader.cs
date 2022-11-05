8ec149fdb11b3e6b0a1a895eb9cd7b3f17306dc8
Viacheslav Rostovtsev <virost@google.com>
2022-11-05 11:16:32 -0700

    cancellation support and misc

--- Google.Api.Gax.Grpc/Rest/PartialDecodingStreamReader.cs
+++ Google.Api.Gax.Grpc/Rest/PartialDecodingStreamReader.cs
@@ -18,12 +18,12 @@ namespace Google.Api.Gax.Grpc.Rest;
 public class PartialDecodingStreamReader<TResponse> : IAsyncStreamReader<TResponse>
 {
     private readonly Func<string, TResponse> _parsing;
-    private readonly StreamReader _sr;
 
     private readonly Queue<TResponse> _readyResults;
     private readonly StringBuilder _currentBuffer;
 
     private bool _arrayClosed;
+    private readonly Stream _stream;
 
     /// <summary>
     /// 
@@ -35,8 +35,9 @@ public class PartialDecodingStreamReader<TResponse> : IAsyncStreamReader<TRespon
         _parsing = parsing;
 
         var httpResponse = httpResponseTask.ConfigureAwait(false).GetAwaiter().GetResult();
-        var stream = httpResponse.Content.ReadAsStreamAsync().ConfigureAwait(false).GetAwaiter().GetResult();
-        _sr = new StreamReader(stream);
+        // TODO [virost, 11/5]: should I wrap this exception?
+        httpResponse.EnsureSuccessStatusCode();
+        _stream = httpResponse.Content.ReadAsStreamAsync().ConfigureAwait(false).GetAwaiter().GetResult();
         
         _readyResults = new Queue<TResponse>();
         _currentBuffer = new StringBuilder();
@@ -50,24 +51,39 @@ public class PartialDecodingStreamReader<TResponse> : IAsyncStreamReader<TRespon
     /// <returns></returns>
     public async Task<bool> MoveNext(CancellationToken cancellationToken)
     {
-        if (_sr.EndOfStream)
-        {
-            return false;
-        }
-
         if (_readyResults.Count > 0)
         {
             Current = _readyResults.Dequeue();
             return true;
         }
 
-        while (_readyResults.Count == 0 && !_sr.EndOfStream)
+        while (_readyResults.Count == 0)
         {
-            var buffer = new char[1000];
-            var s = await _sr.ReadAsync(buffer, 0, 100).ConfigureAwait(false);
-            if (s == 0) continue;
+            // StreamReader.ReadAsync does not have an overload with CancellationToken in 4.6.2, but Stream.ReadAsync does.
+            // TODO [virost, 11/5]: 1000 is completely arbitrary, is there a better number?
+            var buffer = new byte[1000];
 
-            foreach (var c in buffer.Take(s))
+            // TODO [virost, 11/5]: should I wrap whatever this throws?
+            var readLen = await _stream.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false);
+
+            if (readLen == 0)
+            {
+                if (!_arrayClosed)
+                {
+                    var errorText = "Closing bracket not received after iterating through the stream. " +
+                                    "This means that streaming ended without all objects transmitted. " +
+                                    "It is likely a result of server or network error.";
+                    throw new InvalidOperationException(errorText);
+                }
+
+                break;
+            }
+
+            var memStream = new MemoryStream(buffer, 0, readLen);
+            var streamReader = new StreamReader(memStream);
+            var readChars = streamReader.ReadToEnd();
+
+            foreach (var c in readChars)
             {
                 // Closing bracket for the top-level array
                 if (_currentBuffer.Length == 0 && c == ']')
@@ -101,21 +117,12 @@ public class PartialDecodingStreamReader<TResponse> : IAsyncStreamReader<TRespon
             }
         }
 
-        if (_sr.EndOfStream && !_arrayClosed)
-        {
-            var errorText = "Closing bracket not received after iterating through the stream. " +
-                            "This means that streaming ended without all objects transmitted. " +
-                            "It is likely a result of server or network error.";
-            throw new InvalidOperationException(errorText);
-        }
-
         if (!_readyResults.Any())
         {
             return false;
         }
 
         Current = _readyResults.Dequeue();
-        CurrentStr = _currentBuffer.ToString();
         return true;
     }
 
@@ -123,9 +130,4 @@ public class PartialDecodingStreamReader<TResponse> : IAsyncStreamReader<TRespon
     /// 
     /// </summary>
     public TResponse Current { get; private set; }
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    public string CurrentStr { get; private set; }
 }
