fb77b3b94957f310eea29bd133095f0849cbe600
Jon Skeet <jonskeet@google.com>
2022-12-06 17:58:44 +0000

    fix: Fix error handling for server-streaming REGAPIC
    This is slightly fiddly in terms of the status/trailers handling, to the extent that we need to create a synthetic ReadHttpResponseMessage to represent "we've read the headers, it's an error, now we'll read the whole content to provide the error details".
    
    Fixes #655

--- Google.Api.Gax.Grpc.IntegrationTests/Rest/RestChannelTest.cs
+++ Google.Api.Gax.Grpc.IntegrationTests/Rest/RestChannelTest.cs
@@ -6,9 +6,11 @@
  */
 
 using Google.Api.Gax.Grpc.IntegrationTests;
-using Google.Protobuf;
+using Google.Protobuf.Collections;
 using Grpc.Core;
+using Grpc.Core.Utils;
 using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -38,6 +40,31 @@ public class RestChannelTest
         Assert.Equal("Error details", exception.Status.Detail);
     }
 
+    [Fact]
+    public async Task Streaming_Success()
+    {
+        var client = CreateClient();
+        var response = client.ServerStreaming(new SimpleRequest { Name = "test" });
+        var elements = await response.ResponseStream.ToListAsync();
+        var expected = new[]
+        {
+            new SimpleResponse { Name = "test0" },
+            new SimpleResponse { Name = "test1" },
+            new SimpleResponse { Name = "test2" },
+        };
+        Assert.Equal(expected, elements);
+    }
+
+    [Fact]
+    public async Task Streaming_Error()
+    {
+        var client = CreateClient();
+        var response = client.ServerStreaming(new SimpleRequest { Name = "error" });
+        var exception = await Assert.ThrowsAsync<RpcException>(() => response.ResponseStream.MoveNext(default));
+        Assert.Equal(StatusCode.InvalidArgument, exception.StatusCode);
+        Assert.Equal("Error details", exception.Status.Detail);
+    }
+
     private TestServiceClient CreateClient()
     {
         var metadata = TestServiceMetadata.ApiMetadata;
@@ -49,14 +76,18 @@ public class RestChannelTest
 
     private class TestServerHandler : HttpMessageHandler
     {
+        private const string SimplePathPrefix = "/v1/simple/";
+        private const string ServerStreamingPathPrefix = "/v1/serverStreaming/";
+        private const string CustomPathPrefix = "/v1/custom:";
+
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             var uri = request.RequestUri;
             string path = uri.AbsolutePath;
             var response =
-                path.StartsWith("/v1/simple") ? GetSimpleResponse(request)
-                : path.StartsWith("/v1/serverStreaming") ? GetStreamingResponse(request)
-                : path.StartsWith("/v1/custom:") ? GetCustomMethod(request)
+                path.StartsWith(SimplePathPrefix) ? GetSimpleResponse(request)
+                : path.StartsWith(ServerStreamingPathPrefix) ? GetStreamingResponse(request)
+                : path.StartsWith(CustomPathPrefix) ? GetCustomMethod(request)
                 : new HttpResponseMessage { StatusCode = HttpStatusCode.NotFound };
             return Task.FromResult(response);
         }
@@ -65,7 +96,7 @@ public class RestChannelTest
         {
             var uri = request.RequestUri;
             string path = uri.AbsolutePath;
-            string name = Uri.UnescapeDataString(path.Substring("/v1/simple/".Length));
+            string name = Uri.UnescapeDataString(path.Substring(SimplePathPrefix.Length));
             if (name == "error")
             {
                 return CreateErrorResponseMessage(HttpStatusCode.BadRequest,
@@ -76,7 +107,19 @@ public class RestChannelTest
 
         private HttpResponseMessage GetStreamingResponse(HttpRequestMessage request)
         {
-            return new HttpResponseMessage { StatusCode = HttpStatusCode.NotImplemented };
+            var uri = request.RequestUri;
+            string path = uri.AbsolutePath;
+            string name = Uri.UnescapeDataString(path.Substring(ServerStreamingPathPrefix.Length));
+            if (name == "error")
+            {
+                var repeatedError = new RepeatedField<Error> { CreateError(StatusCode.InvalidArgument, "Error details") };
+                return CreateErrorResponseMessage(HttpStatusCode.BadRequest, repeatedError);
+            }
+
+            var responses = Enumerable.Range(0, 3).Select(index => new SimpleResponse { Name = name + index });
+            var repeated = new RepeatedField<SimpleResponse>();
+            repeated.AddRange(responses);
+            return CreateSuccessResponseMessage(repeated);
         }
 
         private HttpResponseMessage GetCustomMethod(HttpRequestMessage request)
@@ -84,7 +127,7 @@ public class RestChannelTest
             return new HttpResponseMessage { StatusCode = HttpStatusCode.NotImplemented };
         }
 
-        private static HttpResponseMessage CreateSuccessResponseMessage(IMessage message)
+        private static HttpResponseMessage CreateSuccessResponseMessage(object message)
         {
             string json = message.ToString();
             return new HttpResponseMessage
@@ -94,9 +137,21 @@ public class RestChannelTest
             };
         }
 
-        private static HttpResponseMessage CreateErrorResponseMessage(HttpStatusCode httpStatusCode, StatusCode gRpcStatusCode, string message)
+        private static HttpResponseMessage CreateErrorResponseMessage(HttpStatusCode httpStatusCode, StatusCode gRpcStatusCode, string message) =>
+            CreateErrorResponseMessage(httpStatusCode, CreateError(gRpcStatusCode, message));
+
+        private static HttpResponseMessage CreateErrorResponseMessage(HttpStatusCode httpStatusCode, object error)
         {
-            var response = new Error
+            string json = error.ToString();
+            return new HttpResponseMessage
+            {
+                StatusCode = httpStatusCode,
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+        }
+
+        private static Error CreateError(StatusCode gRpcStatusCode, string message) =>
+            new Error
             {
                 Error_ = new Error.Types.Status
                 {
@@ -104,13 +159,5 @@ public class RestChannelTest
                     Message = message
                 }
             };
-
-            string json = response.ToString();
-            return new HttpResponseMessage
-            {
-                StatusCode = httpStatusCode,
-                Content = new StringContent(json, Encoding.UTF8, "application/json")
-            };
-        }
     }
 }
