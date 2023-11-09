d24d920dea206c421cfe35d2c83aa2990b9912fa
Jon Skeet <jonskeet@google.com>
2023-11-09 09:14:41 +0000

    chore: Minor refactoring and docs updates for Status handwritten code

--- Google.Api.CommonProtos/Rpc/StatusPartial.cs
+++ Google.Api.CommonProtos/Rpc/StatusPartial.cs
@@ -15,48 +15,44 @@ namespace Google.Rpc;
 public partial class Status
 {
     /// <summary>
-    /// Cache the full names of the messages types
+    /// Cache for the full names of the messages types.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="T">The message type whose name is cached.</typeparam>
     private static class MessageNameCache<T> where T : class, IMessage<T>, new()
     {
-        public static readonly string FullName = new T().Descriptor.FullName;
+        public static string FullName { get; } = new T().Descriptor.FullName;
     }
 
     /// <summary>
-    /// Retrieves the error details of type <typeparamref name="T"/> from the <see cref="Google.Rpc.Status"/>
+    /// Retrieves the error details of type <typeparamref name="T"/> from the <see cref="Status"/>
     /// message.
     /// </summary>
     /// <remarks>
     /// <example>
-    /// For example, to retrieve any <see cref="Google.Rpc.ErrorInfo"/> that might be in the status details:
+    /// For example, to retrieve any <see cref="ErrorInfo"/> that might be in the status details:
     /// <code>
-    ///   var errorInfo = status.GetStatusDetail&lt;ErrorInfo&gt;();
-    ///   if (errorInfo is not null) {
-    ///      // ...
-    ///   }
+    /// var errorInfo = status.GetStatusDetail&lt;ErrorInfo&gt;();
+    /// if (errorInfo is not null)
+    /// {
+    ///    // ...
+    /// }
     /// </code>
     /// </example>
     /// </remarks>
     /// <typeparam name="T">The message type to decode from within the error details.</typeparam>
-    /// <returns>The first error details of type <typeparamref name="T"/> found, or null if not present</returns>
+    /// <returns>The first error details of type <typeparamref name="T"/> found, or null if not present.</returns>
     public T GetDetail<T>() where T : class, IMessage<T>, new()
     {
         var expectedName = MessageNameCache<T>.FullName;
-        var any = Details.FirstOrDefault(a => Any.GetTypeName(a.TypeUrl) == expectedName);
-        if (any is null)
-        {
-            return null;
-        }
-        return any.Unpack<T>();
+        return Details.FirstOrDefault(a => Any.GetTypeName(a.TypeUrl) == expectedName)?.Unpack<T>();
     }
 
     /// <summary>
-    /// Iterate over all the messages in the <see cref="Google.Rpc.Status.Details"/>
+    /// Iterate over all the messages in the <see cref="Details"/>
     /// </summary>
     /// <remarks>
     /// <para>
-    /// Iterate over the messages in the <see cref="Google.Rpc.Status.Details"/> that are messages
+    /// Iterate over the messages in the <see cref="Details"/> that are messages
     /// in the <see href="https://github.com/googleapis/googleapis/blob/master/google/rpc/error_details.proto">
     /// standard set of error types</see> defined in the richer error model. Any other messages found in
     /// the Details are ignored and not returned.
@@ -65,31 +61,32 @@ public partial class Status
     /// <example>
     /// Example:
     /// <code>
-    /// foreach (var msg in status.UnpackDetailMessages()) {
-    ///   switch (msg) {
-    ///     case ErrorInfo errorInfo:
-    ///          // Handle errorInfo ...
-    ///          break;
-    ///     // Other cases ...
-    ///   }
+    /// foreach (var msg in status.UnpackDetailMessages())
+    /// {
+    ///     switch (msg)
+    ///     {
+    ///         case ErrorInfo errorInfo:
+    ///             // Handle errorInfo ...
+    ///             break;
+    ///
+    ///         // Other cases ...
+    ///     }
     /// }
     /// </code>
     /// </example>
     /// </para>
     /// </remarks>
     /// <returns></returns>
-    public IEnumerable<IMessage> UnpackDetailMessages()
-    {
-        return UnpackDetailMessages(StandardErrorTypeRegistry.Registry);
-    }
+    public IEnumerable<IMessage> UnpackDetailMessages() =>
+        UnpackDetailMessages(StandardErrorTypeRegistry.Registry);
 
     /// <summary>
-    /// Iterate over all the messages in the <see cref="Google.Rpc.Status.Details"/> that match types
+    /// Iterate over all the messages in the <see cref="Details"/> that match types
     /// in the given <see cref="TypeRegistry"/>
     /// </summary>
     /// <remarks>
     /// <para>
-    /// Iterate over the messages in the <see cref="Google.Rpc.Status.Details"/> that are messages
+    /// Iterate over the messages in the <see cref="Details"/> that are messages
     /// in the given <see cref="TypeRegistry"/>. Any other messages found in the Details are ignored
     /// and not returned.  This allows iterating over custom messages if you are not using the
     /// standard set of error types defined in the rich error model.
@@ -98,34 +95,25 @@ public partial class Status
     /// <example>
     /// Example:
     /// <code>
-    /// TypeRegistry myTypes = TypeRegistry.FromMessages(
-    ///   new MessageDescriptor[] {
-    ///     FooMessage.Descriptor, BarMessage.Descriptor
-    ///   });
+    /// TypeRegistry myTypes = TypeRegistry.FromMessages(FooMessage.Descriptor, BarMessage.Descriptor);
     ///   
-    /// foreach (var msg in status.UnpackDetailMessages(myTypes)) {
-    ///   switch (msg) {
-    ///     case FooMessage foo:
-    ///          // Handle foo ...
-    ///          break;
-    ///     // Other cases ...
-    ///   }
+    /// foreach (var msg in status.UnpackDetailMessages(myTypes))
+    /// {
+    ///     switch (msg)
+    ///     {
+    ///         case FooMessage foo:
+    ///             // Handle foo ...
+    ///              break;
+    ///
+    ///         // Other cases ...
+    ///     }
     /// }
     /// </code>
     /// </example>
     /// </para>
     /// </remarks>
-    /// <param name="registry"></param>
-    /// <returns></returns>
-    public IEnumerable<IMessage> UnpackDetailMessages(TypeRegistry registry)
-    {
-        foreach (var any in Details)
-        {
-            var msg = any.Unpack(registry);
-            if (!(msg is null))
-            {
-                yield return msg;
-            }
-        }
-    }
+    /// <param name="registry">The type registry to use to unpack detail messages.</param>
+    /// <returns>A (possibly-empty) sequence of detail messages.</returns>
+    public IEnumerable<IMessage> UnpackDetailMessages(TypeRegistry registry) =>
+        Details.Select(any => any.Unpack(registry)).Where(msg => msg is not null);
 }
