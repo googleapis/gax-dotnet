47d0e7f4a0aa700b23679be76dedd39ac7fe13d5
Jon Skeet <jonskeet@google.com>
2023-11-09 09:15:12 +0000

    tests: Refactor Status test and add test for custom detail types

--- Google.Api.CommonProtos.Tests/Rpc/StatusTest.cs
+++ Google.Api.CommonProtos.Tests/Rpc/StatusTest.cs
@@ -4,367 +4,198 @@
  * license that can be found in the LICENSE file or at
  * https://developers.google.com/open-source/licenses/bsd
  */
+using Google.Api;
 using Google.Protobuf;
+using Google.Protobuf.Reflection;
 using Google.Protobuf.WellKnownTypes;
 using System;
-using System.Collections.Generic;
+using System.Linq;
 using Xunit;
+using static Google.Rpc.BadRequest.Types;
+using static Google.Rpc.Help.Types;
 
 namespace Google.Rpc;
 
 public class StatusTest
 {
     [Fact]
-    public void GetStatusDetailTest()
+    public void GetStatusDetail_AllDetails()
     {
-        // Arrange - create a status
-        // The detailsMap contains all the Messages added to the status so
-        // these can be used in the comparisions when then are retrieved later
-        var detailsMap = new Dictionary<string, IMessage>();
-        var status = CreateFullStatus(detailsMap);
+        var status = CreateFullStatus();
+        AssertDetail(CreateBadRequest());
+        AssertDetail(CreateErrorInfo());
+        AssertDetail(CreateRetryInfo());
+        AssertDetail(CreateDebugInfo());
+        AssertDetail(CreateQuotaFailure());
+        AssertDetail(CreatePreconditionFailure());
+        AssertDetail(CreateRequestInfo());
+        AssertDetail(CreateHelp());
+        AssertDetail(CreateLocalizedMessage());
 
-        // Act
-        var badRequest = status.GetDetail<BadRequest>();
-        // Assert
-        Assert.NotNull(badRequest);
-        var expected = detailsMap["badRequest"];
-        Assert.Equal(expected, badRequest);
-
-        // Act
-        var errorInfo = status.GetDetail<ErrorInfo>();
-        // Assert
-        Assert.NotNull(errorInfo);
-        expected = detailsMap["errorInfo"];
-        Assert.Equal(expected, errorInfo);
-
-        // Act
-        var retryInfo = status.GetDetail<RetryInfo>();
-        // Assert
-        Assert.NotNull(retryInfo);
-        expected = detailsMap["retryInfo"];
-        Assert.Equal(expected, retryInfo);
-
-        // Act
-        var debugInfo = status.GetDetail<DebugInfo>();
-        // Assert
-        Assert.NotNull(debugInfo);
-        expected = detailsMap["debugInfo"];
-        Assert.Equal(expected, debugInfo);
-
-        // Act
-        var quotaFailure = status.GetDetail<QuotaFailure>();
-        // Assert
-        Assert.NotNull(quotaFailure);
-        expected = detailsMap["quotaFailure"];
-        Assert.Equal(expected, quotaFailure);
-
-        // Act
-        var preconditionFailure = status.GetDetail<PreconditionFailure>();
-        // Assert
-        Assert.NotNull(preconditionFailure);
-        expected = detailsMap["preconditionFailure"];
-        Assert.Equal(expected, preconditionFailure);
-
-        // Act
-        var requestInfo = status.GetDetail<RequestInfo>();
-        // Assert
-        Assert.NotNull(requestInfo);
-        expected = detailsMap["requestInfo"];
-        Assert.Equal(expected, requestInfo);
-
-        // Act
-        var help = status.GetDetail<Help>();
-        // Assert
-        Assert.NotNull(help);
-        expected = detailsMap["help"];
-        Assert.Equal(expected, help);
-
-        // Act
-        var localizedMessage = status.GetDetail<LocalizedMessage>();
-        // Assert
-        Assert.NotNull(localizedMessage);
-        expected = detailsMap["localizedMessage"];
-        Assert.Equal(expected, localizedMessage);
+        void AssertDetail<T>(T expected) where T : class, IMessage<T>, new()
+        {
+            var actual = status.GetDetail<T>();
+            Assert.NotNull(actual);
+            Assert.Equal(expected, actual);
+        }
     }
 
     [Fact]
     public void GetStatusDetail_NotFound()
     {
-        // Arrange - create a status with only a few details
-        // The detailsMap contains all the Messages added to the status so
-        // these can be used in the comparisions when then are retrieved later
-        var detailsMap = new Dictionary<string, IMessage>();
-        var status = CreatePartialStatus(detailsMap);
-
-        // Act - try and retieve non-existent BadRequest from the status
-        var badRequest = status.GetDetail<BadRequest>();
-        // Assert
-        Assert.Null(badRequest);
+        var status = CreatePartialStatus();
+        Assert.Null(status.GetDetail<BadRequest>());
     }
 
     [Fact]
-    public void UnpackDetailMessageTest()
+    public void UnpackDetailMessages()
     {
-        // Arrange - create a status
-        // The detailsMap contains all the Messages added to the status so
-        // these can be used in the comparisions when then are retrieved later
-        var detailsMap = new Dictionary<string, IMessage>();
-        var status = CreateFullStatus(detailsMap);
+        var status = CreateFullStatus();
 
-        // foundSet will contain the messages found in the status so we can
-        // check all those expected were present
-        var foundSet = new HashSet<string>();
+        var detailList = status.UnpackDetailMessages().ToList();
 
-        // Act and Assert - iterate over all the messages in the status
-        // and check they contain what is expected
-        foreach (var msg in status.UnpackDetailMessages())
-        {
-            switch (msg)
-            {
-                case ErrorInfo errorInfo:
-                    {
-                        var expected = detailsMap["errorInfo"];
-                        Assert.Equal(expected, errorInfo);
-                        foundSet.Add("errorInfo");
-                        break;
-                    }
+        // All the details should be returned by UnpackDetailMessages because we
+        // know about all of them.
+        Assert.Equal(status.Details.Count, detailList.Count);
 
-                case BadRequest badRequest:
-                    {
-                        var expected = detailsMap["badRequest"];
-                        Assert.Equal(expected, badRequest);
-                        foundSet.Add("badRequest");
-                        break;
-                    }
-
-                case RetryInfo retryInfo:
-                    {
-                        var expected = detailsMap["retryInfo"];
-                        Assert.Equal(expected, retryInfo);
-                        foundSet.Add("retryInfo");
-                        break;
-                    }
-
-                case DebugInfo debugInfo:
-                    {
-                        var expected = detailsMap["debugInfo"];
-                        Assert.Equal(expected, debugInfo);
-                        foundSet.Add("debugInfo");
-                        break;
-                    }
-
-                case QuotaFailure quotaFailure:
-                    {
-                        var expected = detailsMap["quotaFailure"];
-                        Assert.Equal(expected, quotaFailure);
-                        foundSet.Add("quotaFailure");
-                        break;
-                    }
-
-                case PreconditionFailure preconditionFailure:
-                    {
-                        var expected = detailsMap["preconditionFailure"];
-                        Assert.Equal(expected, preconditionFailure);
-                        foundSet.Add("preconditionFailure");
-                        break;
-                    }
-
-                case RequestInfo requestInfo:
-                    {
-                        var expected = detailsMap["requestInfo"];
-                        Assert.Equal(expected, requestInfo);
-                        foundSet.Add("requestInfo");
-                        break;
-                    }
-
-                case ResourceInfo resourceInfo:
-                    {
-                        var expected = detailsMap["resourceInfo"];
-                        Assert.Equal(expected, resourceInfo);
-                        foundSet.Add("resourceInfo");
-                        break;
-                    }
-
-                case Help help:
-                    {
-                        var expected = detailsMap["help"];
-                        Assert.Equal(expected, help);
-                        foundSet.Add("help");
-                        break;
-                    }
-
-                case LocalizedMessage localizedMessage:
-                    {
-                        var expected = detailsMap["localizedMessage"];
-                        Assert.Equal(expected, localizedMessage);
-                        foundSet.Add("localizedMessage");
-                        break;
-                    }
-            }
-        }
-
-        // check everything was returned
-        Assert.Equal(detailsMap.Count, foundSet.Count);
-
+        Assert.Contains(CreateBadRequest(), detailList);
+        Assert.Contains(CreateErrorInfo(), detailList);
+        Assert.Contains(CreateRetryInfo(), detailList);
+        Assert.Contains(CreateDebugInfo(), detailList);
+        Assert.Contains(CreateQuotaFailure(), detailList);
+        Assert.Contains(CreatePreconditionFailure(), detailList);
+        Assert.Contains(CreateRequestInfo(), detailList);
+        Assert.Contains(CreateHelp(), detailList);
+        Assert.Contains(CreateLocalizedMessage(), detailList);
     }
 
-    private static Google.Rpc.Status CreatePartialStatus(Dictionary<string, IMessage> detailsMap = null)
+    [Fact]
+    public void NonStandardDetails()
     {
-        var retryInfo = new RetryInfo
-        {
-            RetryDelay = Duration.FromTimeSpan(new TimeSpan(0, 0, 5))
-        };
+        // Clearly this won't actually be in a status, but it's
+        // just "a message we don't have in the registry".
+        var generationSettings = new DotnetSettings { IgnoredResources = { "ignored" } };
 
-        var debugInfo = new DebugInfo()
+        var status = new Status
         {
-            StackEntries = { "stack1", "stack2" },
-            Detail = "detail"
-        };
-
-        // add details to a map for later checking
-        if (detailsMap != null)
-        {
-            detailsMap.Clear();
-            detailsMap.Add("retryInfo", retryInfo);
-            detailsMap.Add("debugInfo", debugInfo);
-        }
-
-        var status = new Google.Rpc.Status()
-        {
-            Code = (int)Code.Unavailable,
-            Message = "partial status",
             Details =
-        {
-            Any.Pack(retryInfo),
-            Any.Pack(debugInfo),
-        }
+            {
+                Any.Pack(CreateBadRequest()),
+                Any.Pack(generationSettings)
+            }
         };
 
-        return status;
+        Assert.Equal(CreateBadRequest(), status.GetDetail<BadRequest>());
+        Assert.Equal(generationSettings, status.GetDetail<DotnetSettings>());
+
+        // Standard type registry...
+        var detailList = status.UnpackDetailMessages().ToList();
+        Assert.Equal(1, detailList.Count());
+        Assert.Contains(CreateBadRequest(), detailList);
+        Assert.DoesNotContain(generationSettings, detailList);
+
+        // Custom type registry... (no standard details)
+        var customTypeRegistry = TypeRegistry.FromFiles(ClientReflection.Descriptor);
+        detailList = status.UnpackDetailMessages(customTypeRegistry).ToList();
+        Assert.Equal(1, detailList.Count());
+        Assert.DoesNotContain(CreateBadRequest(), detailList);
+        Assert.Contains(generationSettings, detailList);
     }
 
-    static Google.Rpc.Status CreateFullStatus(Dictionary<string, IMessage> detailsMap = null)
+    private static Status CreatePartialStatus() => new()
     {
-        var errorInfo = new ErrorInfo()
+        Code = (int) Code.Unavailable,
+        Message = "partial status",
+        Details =
         {
-            Domain = "Rich Error Model Demo",
-            Reason = "Full error requested in the demo",
-            Metadata =
+            Any.Pack(CreateRetryInfo()),
+            Any.Pack(CreateDebugInfo()),
+        }
+    };
+
+    private static Status CreateFullStatus() => new()
+    {
+        Code = (int) Code.ResourceExhausted,
+        Message = "Test",
+        Details =
+        {
+            Any.Pack(CreateBadRequest()),
+            Any.Pack(CreateErrorInfo()),
+            Any.Pack(CreateRetryInfo()),
+            Any.Pack(CreateDebugInfo()),
+            Any.Pack(CreateQuotaFailure()),
+            Any.Pack(CreatePreconditionFailure()),
+            Any.Pack(CreateRequestInfo()),
+            Any.Pack(CreateResourceInfo()),
+            Any.Pack(CreateHelp()),
+            Any.Pack(CreateLocalizedMessage())
+        }
+    };
+
+    private static BadRequest CreateBadRequest() => new()
+    {
+        FieldViolations = { new FieldViolation { Field = "field", Description = "description" } }
+    };
+
+    private static RetryInfo CreateRetryInfo() => new()
+    {
+        RetryDelay = Duration.FromTimeSpan(new TimeSpan(0, 0, 5))
+    };
+
+    private static DebugInfo CreateDebugInfo() => new()
+    {
+        StackEntries = { "stack1", "stack2" },
+        Detail = "detail"
+    };
+
+    private static QuotaFailure CreateQuotaFailure() => new()
+    {
+        Violations = { new QuotaFailure.Types.Violation { Description = "Too much disk space used", Subject = "Disk23" } }
+    };
+
+    private static PreconditionFailure CreatePreconditionFailure() => new()
+    {
+        Violations =
+        {
+            new PreconditionFailure.Types.Violation
             {
-                { "key1", "value1" },
-                { "key2", "value2" }
-            }
-        };
-
-        var badRequest = new BadRequest()
-        {
-            FieldViolations =
-        {
-            new BadRequest.Types.FieldViolation()
-            {
-                Field = "field", Description = "description"
+                Type = "type",
+                Subject = "subject",
+                Description = "description"
             }
         }
-        };
+    };
 
-        var retryInfo = new RetryInfo
-        {
-            RetryDelay = Duration.FromTimeSpan(new TimeSpan(0, 0, 5))
-        };
+    private static RequestInfo CreateRequestInfo() => new() { RequestId = "reqId", ServingData = "data" };
 
-        var debugInfo = new DebugInfo()
-        {
-            StackEntries = { "stack1", "stack2" },
-            Detail = "detail"
-        };
+    private static ResourceInfo CreateResourceInfo() => new()
+    {
+        ResourceType = "type",
+        ResourceName = "name",
+        Owner = "owner",
+        Description = "description"
+    };
 
-        var quotaFailure = new QuotaFailure()
+    private static Help CreateHelp() => new Help
+    {
+        Links =
         {
-            Violations =
-        {
-            new QuotaFailure.Types.Violation()
-            {
-                Description =  "Too much disk space used",
-                Subject = "Disk23"
-            }
+            new Link { Url = "url1", Description = "desc1" },
+            new Link { Url = "url2", Description = "desc2" },
         }
-        };
+    };
 
-        var preconditionFailure = new PreconditionFailure()
+    private static LocalizedMessage CreateLocalizedMessage() =>
+        new() { Locale = "en-GB", Message = "Example localised error message" };
+
+    private static ErrorInfo CreateErrorInfo() => new()
+    {
+        Domain = "Rich Error Model Demo",
+        Reason = "Full error requested in the demo",
+        Metadata =
         {
-            Violations =
-        {
-            new PreconditionFailure.Types.Violation()
-            {
-                Type = "type", Subject = "subject", Description = "description"
-            }
+            { "key1", "value1" },
+            { "key2", "value2" }
         }
-        };
-
-        var requestInfo = new RequestInfo()
-        {
-            RequestId = "reqId",
-            ServingData = "data"
-        };
-
-        var resourceInfo = new ResourceInfo()
-        {
-            ResourceType = "type",
-            ResourceName = "name",
-            Owner = "owner",
-            Description = "description"
-        };
-
-        var help = new Help()
-        {
-            Links =
-        {
-            new Help.Types.Link() { Url="url1", Description="desc1" },
-            new Help.Types.Link() { Url="url2", Description="desc2" },
-        }
-        };
-
-        var localizedMessage = new LocalizedMessage()
-        {
-            Locale = "en-GB",
-            Message = "Example localised error message"
-        };
-
-        // add details to a map for later checking
-        if (detailsMap != null)
-        {
-            detailsMap.Clear();
-            detailsMap.Add("badRequest", badRequest);
-            detailsMap.Add("errorInfo", errorInfo);
-            detailsMap.Add("retryInfo", retryInfo);
-            detailsMap.Add("debugInfo", debugInfo);
-            detailsMap.Add("quotaFailure", quotaFailure);
-            detailsMap.Add("preconditionFailure", preconditionFailure);
-            detailsMap.Add("requestInfo", requestInfo);
-            detailsMap.Add("resourceInfo", resourceInfo);
-            detailsMap.Add("help", help);
-            detailsMap.Add("localizedMessage", localizedMessage);
-        }
-
-        var status = new Google.Rpc.Status()
-        {
-            Code = (int)Code.ResourceExhausted,
-            Message = "Test",
-            Details =
-        {
-            Any.Pack(badRequest),
-            Any.Pack(errorInfo),
-            Any.Pack(retryInfo),
-            Any.Pack(debugInfo),
-            Any.Pack(quotaFailure),
-            Any.Pack(preconditionFailure),
-            Any.Pack(requestInfo),
-            Any.Pack(resourceInfo),
-            Any.Pack(help),
-            Any.Pack(localizedMessage)
-        }
-        };
-
-        return status;
-    }
+    };
 }
