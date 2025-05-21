615f639998fb7bf1da8b3c04d053163bc4983d91
purvav <purva9413@gmail.com>
2025-05-21 11:02:21 -0700

    chore: Modify release scripts and remove kokoro configs (#823)
    * chore: Modify release scripts and remove kokoro configs
    
    * fix: Fix source link
    
    * fix: Remove unnecesart condition and fix sample
    
    * chore: Update description
    
    * fix: Fix typos in vars
    
    ---------
    
    Co-authored-by: Purva Vasudeo <purvavasudeo@google.com>

--- buildrelease.sh
+++ buildrelease.sh
@@ -1,37 +1,22 @@
 #!/bin/bash
 
 # Script to perform a release build for GAX, based on
-# a commit for which tags have been created.
+# HEAD commit for which tags have been created.
 # This builds both GAX and the common protos; but will delete
-# all built packages that don't match one of the tags in the commit
+# all built packages that don't match one of the tags in the HEAD commit
 # hash. For instance, if there's only one tag in the commit hash 
 # that starts with Google.Api.Gax.Grpc then only the following nuget
 # packages will remain after the script is done:
 # Google.Api.Gax.Grpc.<version>.nupkg
 # Google.Api.Gax.Grpc.Gcp.<version>.nupkg
 # Google.Api.Gax.Grpc.Testing.<version>.nupkg
-# Sample: buildrelease.sh 847118821d8a8b4ac2ea3bbe692acea29c14c4da
-
-# The repo is cloned into a fresh "releasebuild" directory.
 
 set -e
 
-if [ -z "$1" ]
-then
-  echo "Please specify a commit hash."
-  exit 1
-fi
-
-commit=$1
+commit=$(git rev-parse HEAD)
 
 # Do everything from the repository root for sanity.
 cd $(dirname $0)
-
-rm -rf releasebuild
-git clone https://github.com/googleapis/gax-dotnet.git releasebuild -c core.autocrlf=input
-
-cd releasebuild
-git checkout $commit
 
 ./build.sh
 
@@ -70,4 +55,4 @@ do
   fi
 done
 
-echo "Build complete. Push packages from releasebuild/nuget."
+echo "Build complete. Push packages from ./nuget."
