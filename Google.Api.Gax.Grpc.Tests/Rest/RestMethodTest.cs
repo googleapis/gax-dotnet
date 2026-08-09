6c5f94beebf7f32310ceba8c36bc2d642c81a591
Amanda Tarafa Mas <atarafamas@google.com>
2026-08-09 15:14:09 -0700

    feat: RestMethod.Create supports resumable uploads.

--- Google.Api.Gax.Grpc.Tests/Rest/RestMethodTest.cs
+++ Google.Api.Gax.Grpc.Tests/Rest/RestMethodTest.cs
@@ -12,6 +12,7 @@ using Grpc.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using Xunit;
 
 namespace Google.Api.Gax.Grpc.Rest.Tests;
@@ -104,8 +105,81 @@ public class RestMethodTest
         Assert.Equal(expectedPrefix, prefix);
     }
 
+    [Fact]
+    public void Create_ResumableUploadMethod_WithPrefix()
+    {
+        var methodDescriptor = CreateResumableUploadMethodDescriptor(includeHttpOption: true);
+        var apiMetadata = TestApiMetadata.Test;
+        var methods = RestMethod.Create(apiMetadata, methodDescriptor, JsonParser.Default).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+        Assert.Equal(2, methods.Count);
+        Assert.True(methods.ContainsKey("/google.showcase.v1beta1.ResumableUploadService/UploadMedia#start"));
+        Assert.True(methods.ContainsKey("/google.showcase.v1beta1.ResumableUploadService/UploadMedia#started"));
+
+        var startMethod = methods["/google.showcase.v1beta1.ResumableUploadService/UploadMedia#start"];
+        var startedMethod = methods["/google.showcase.v1beta1.ResumableUploadService/UploadMedia#started"];
+        Assert.NotNull(startMethod);
+        Assert.NotNull(startedMethod);
+
+        // Verify startMethod HTTP request
+        var startRequest = new SimpleRequest { Name = "test" };
+        var startHttpRequest = startMethod.CreateRequest(startRequest, host: null);
+        Assert.Equal("/resumable/upload/v1/media/upload", startHttpRequest.RequestUri.ToString());
+        Assert.Equal(HttpMethod.Post, startHttpRequest.Method);
+
+        // Verify startedMethod HTTP request
+        var uploadRequest = new ResumableUploadRequest(new Uri("http://localhost/upload/123"));
+        var startedHttpRequest = startedMethod.CreateRequest(uploadRequest, host: null);
+        Assert.Equal("http://localhost/upload/123", startedHttpRequest.RequestUri.ToString());
+        Assert.Equal(HttpMethod.Post, startedHttpRequest.Method);
+    }
+
+    [Fact]
+    public void Create_ResumableUploadMethod_WithoutHttpRule()
+    {
+        var methodDescriptor = CreateResumableUploadMethodDescriptor(includeHttpOption: false);
+        var apiMetadata = TestApiMetadata.Test;
+        var pair = Assert.Single(RestMethod.Create(apiMetadata, methodDescriptor, JsonParser.Default));
+
+        Assert.Equal("/google.showcase.v1beta1.ResumableUploadService/UploadMedia", pair.Key);
+        Assert.Null(pair.Value);
+    }
+
+    private static MethodDescriptor CreateResumableUploadMethodDescriptor(bool includeHttpOption)
+    {
+        var methodProto = new MethodDescriptorProto
+        {
+            Name = "UploadMedia",
+            InputType = ".google.api.gax.grpc.rest.tests.SimpleRequest",
+            OutputType = ".google.api.gax.grpc.rest.tests.SimpleResponse",
+        };
+        if (includeHttpOption)
+        {
+            methodProto.Options = new MethodOptions();
+            methodProto.Options.SetExtension(AnnotationsExtensions.Http, new HttpRule { Post = "/v1/media/upload", Body = "*" });
+        }
+
+        var fileProto = new FileDescriptorProto
+        {
+            Name = "showcase.proto",
+            Package = "google.showcase.v1beta1",
+            Dependency = { TestServiceReflection.Descriptor.Name, HttpRule.Descriptor.File.Name },
+            Service =
+            {
+                new ServiceDescriptorProto
+                {
+                    Name = "ResumableUploadService",
+                    Method = { methodProto }
+                }
+            }
+        };
+
+        byte[] bytes = fileProto.ToByteArray();
+        var fileDescriptor = FileDescriptor.FromGeneratedCode(bytes, new[] { TestServiceReflection.Descriptor, HttpRule.Descriptor.File }, new GeneratedClrTypeInfo(null, null, null));
+        return Assert.Single(fileDescriptor.Services, s => s.Name == "ResumableUploadService").FindMethodByName("UploadMedia");
+    }
+
     private static MethodDescriptor GetMethod(string service, string method) =>
-        TestServiceReflection.Descriptor.Services
-            .Single(svc => svc.Name == service)
+        Assert.Single(TestServiceReflection.Descriptor.Services, svc => svc.Name == service)
             .FindMethodByName(method);
 }
